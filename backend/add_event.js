407ea770d54e1d7431178c9404931824fdbc352d
tommyye04 <tye47@uwo.ca>
2024-01-13 13:49:00 -0500

    cleaning code update

--- backend/add_event.js
+++ backend/add_event.js
@@ -12,30 +12,4 @@ function getData() {
   console.log(intensity_value);
   console.log(type_value);
   console.log(date_value);
-
- 
-}
-
-function checkForm() {
-  const name_value = document.getElementById("name").value;
-  const long_value = document.getElementById("long").value;
-  const lat_value = document.getElementById("lat").value;
-  const intensity_value = document.getElementById("intensity").value;
-  const type_value = document.getElementById("type").value;
-  const date_value = document.getElementById("date").value;
-
-  const add_button = document.getElementById("add");
-
-  if (
-    name_value.trim() !== "" &&
-    long_value.trim() !== "" &&
-    lat_value.trim() !== "" &&
-    intensity_value.trim() !== "" &&
-    type_value.trim() !== "" &&
-    date_value.trim() !== ""
-  ) {
-    add_button.removeAttribute("disabled");
-  } else {
-    add_button.setAttribute("disabled", "disabled");
-  }
 }
